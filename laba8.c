8503679b90f8476b4d7517e508ebc08f5d34bdba
Anna Rozhnova <32621877+MyuTanako@users.noreply.github.com>
2018-12-15 22:59:23 +0300

    Update laba8.c

--- laba8.c
+++ laba8.c
@@ -1,88 +1,81 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-// определение символических констант
+#include <stdlib.h>
 #define YES 1
 #define NO 0
 #define MAXLINE 1000
-void process_line( char buffer[] );
-void main( void )
+void process_line(char buffer[]);
+void main(void)
 {
 	char line[MAXLINE];
-	gets( line );
-	process_line( line );
-	puts( line );
+	gets(line);
+	process_line(line);
+	puts(line);
+	system("pause");
 }
-
-void process_line( char buffer[] )
+void process_line(char buffer[])
 {
-	char *buf_ptr; // указатель на текущий символ буфера
-	char *end; // указатель на конец слова
-	char *begin; // указатель на начало слова
 	char c; // текущий символ
+	char prev_c; // предыдущий символ
 	int flag; // признак слова
 	int found; // признак того, что буквы повторились
-	// начальные присваивания (инициализация переменных)
-	int counter = -1;
-	int i, plus;
-	char *match[100];
+	int i=0; // позиция текущего символа
+	char *mass[100];
+	char *start; // позиция начала слова
+	char *stop; // позиция конца слова
+	char *ptr, *cptr, *cstart;
+	char copyLine[MAXLINE];
+	int j, k;
 	flag = NO;
 	found = NO;
-	buf_ptr = buffer;
-	begin = buffer;
-	end = buffer;
-	// цикл чтения символов из строки
-	for(i)
+	prev_c = ' ';
+	start = buffer;
+	stop = buffer;
+	ptr = buffer;
+	cstart = copyLine;
+	cptr = copyLine;
+	i = 0;
+	strcpy(copyLine, buffer);
 	do
 	{
-		c = *buf_ptr; // взять текущий символ из буфера
-		if( c == ' ' || c == '.' || c == ',' || c == '\n' || c == '\0')
+		c = *cptr; // взять текущий символ из буфера
+		if (c == ' ' || c == '.' || c == ',' || c == '\n' || c == '\0')
 		{
 			// найден разделитель
-			if( flag == YES )
+			if (flag == YES)
 			{
 				// найден первый разделить после слова
-				end = buf_ptr - 1; // запомнить указатель на
-				// конец слова
-				while(match[counter] !='\0')
-				for(i=0;i<counter-1;i++);
+				stop = ptr - 1; // запомнить позицию конца слова
+				*cptr = '\0';
+				for (j = 0; j < i; j++)
 				{
-					plus = (strcmp(match[i], match[counter]));
-					if (plus == 0)
-						found = YES;
-					else
+					if (!strcmp(cstart, mass[j]))
 					{
-						if(match[i+1]<match[i]+plus)
-						{
-							found = YES;
-						}
+						found = YES;
 					}
 				}
-				if( found == YES )
+				mass[i++] = cstart;
+				if (found == YES)
 				{
-					// две последних буквы в слове были одинаковы
-					// слово нужно удалить
-					char *src = end + 1;
-					char *dst = begin;
-					// удаление слова (перенос оставшейся части
-					// строки)
-					while( (*dst++ = *src++ ) != '\0' );
-					buf_ptr = begin; // начать обработку строки с
-					// перенесённого фрагмента
+					char *src = stop + 1;
+					char *dst = start;
+					while ((*dst++ = *src++) != '\0');
+					ptr = start;
 				}
-			flag = NO;
-			found = NO;
 			}
+			found = NO;
+			flag = NO;
 		}
 		else
 		{
-			// найдена буква
-			if( flag == NO ){
-				begin = buf_ptr; // запомнить указатель на начало
-				counter++; // Считаем кол-во слов
-				match[counter] = buf_ptr;
+			if (flag == NO) 
+			{
+				start = ptr;
+				cstart = cptr;
 			}
 			flag = YES;
 		}
-		buf_ptr++; // продвинуть вперёд указатель на текущий символ
-	}
-	while( c != '\0' );
+		cptr++;
+		ptr++;
+	} while (c != '\0');
 }
